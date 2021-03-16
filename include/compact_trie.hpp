90a53672b8e7b7e21a777b88d6ee0cf76e07f81e
Daniela Campos <daniela@Danielas-MacBook-Pro.local>
2021-03-16 20:58:58 -0300

    All trie iterator operations are implemented

--- include/compact_trie.hpp
+++ include/compact_trie.hpp
@@ -4,6 +4,7 @@
 #include <sdsl/vectors.hpp>
 #include <vector>
 #include <iostream>
+#include <sdsl/bp_support.hpp>
 
 using namespace std;
 using namespace sdsl;
@@ -13,53 +14,155 @@ typedef unsigned int u_int;
 class CompactTrie{
 
     private:
-        u_int key;
-        bool atEnd;
+        bool at_end;
+        bool at_root;
+        u_int it_nodes;
+        u_int it_keys;
+
         bit_vector B;
         vector<u_int> S;
         rank_support_v<1> b_rank1; 
         rank_support_v<0> b_rank0;
         select_support_mcl<0> b_sel0;
         select_support_mcl<1> b_sel1;
+        bp_support_gg<> bp;
+
+        u_int accessS(int it){
+            return S[b_rank1(it+1)-1];
+        }
+
+        u_int succ0(u_int it){
+            u_int cant_0 = b_rank0(it);
+            return b_sel0(cant_0 + 1);
+        }
+
+        u_int succ1(u_int it){
+            u_int cant_1 = b_rank1(it);
+            return b_sel1(cant_1 + 1);
+        }
+
+        u_int prev0(u_int it){
+            u_int cant_0 = b_rank0(it);
+            return b_sel0(cant_0);
+        }
+
+        u_int prev1(u_int it){
+            u_int cant_1 = b_rank1(it);
+            return b_sel1(cant_1);
+        }
+
+        //recives it_key and identifies which child it is
+        u_int childId(u_int it){
+            u_int prev_rank = b_rank1(prev0(it));
+            u_int current_rank = b_rank1(it+1);
+            return current_rank - prev_rank;
+        }
+
+        //gives position of the subtree asociated with the child number num
+        u_int child(u_int it, u_int num){
+            return bp.find_close(succ0(it) - num) + 1;
+        }
+
+        //returns amount of children of node identifies by it_keys
+        u_int children(u_int it_key){
+            return b_rank1(succ0(it_key)) - b_rank1(prev0(it_key));
+        }
+
+        //returns position of subtree asociated with parent of current node
+        u_int parent(u_int it){
+            return prev0(bp.find_open(prev0(it))) + 1;
+        }
+
+        //recives it_nodes and identifies which child it is
+        u_int childRank(u_int it){
+            u_int p = bp.find_open(prev0(it));
+            return succ0(p) - p;
+        }
 
     public:
-        // CompactTrie(bit_vector B, vector<u_int> S);
-        CompactTrie(int val){
-            cout<<"el val es "<<val<<endl;
-        }
 
         CompactTrie(bit_vector b, vector<u_int> s){
             B = b;
             S = s;
+            at_root = true;
+            it_nodes = 3;
+            it_keys = 1;
+            at_end = false;
             util::init_support(b_rank1,&B);
-            // rank_support_v<1> b_rank1(&B); 
-        // rank_support_v<0> b_rank0(&B);
-        // select_support_mcl<0> b_sel0(&B);
-        // select_support_mcl<1> b_sel1(&B);
+            util::init_support(b_rank0,&B);
+            util::init_support(b_sel1,&B);
+            util::init_support(b_sel0,&B);
+            util::init_support(bp, &B);
+        }
+    
+        void open(){
+            if(at_root){
+                at_root = false;
+            }
+
+            it_keys = it_nodes;
+            it_nodes = succ0(it_nodes) + 1;
+            at_end = false;    
         }
 
-        void access_S(int i){
-            cout<<S[i]<<endl;
+        u_int key(){
+            if(at_end){
+                throw "Iterator is atEnd";
+            }
+            return accessS(it_keys);
         }
 
+        void next(){
+            //pending checking for at end
+            u_int child_id = childId(it_keys);
+            u_int cant_children = children(it_keys);
 
+            if(child_id==cant_children){
+                at_end = true;
+            }
+            else{
+                it_keys++;
+                child_id = childId(it_keys);
+                it_nodes = child(it_keys, child_id);
+            }
+        }
+
+        bool atEnd(){
+            return at_end;
+        }
+
+        void showValues(){
+            cout<<"it_nodes es "<<it_nodes<<endl;
+            cout<<"it_key es "<<it_keys<<endl;
+        }
+
+        void up(){
+            it_nodes = parent(it_nodes);
+            u_int child_rank = childRank(it_nodes);
+            u_int grandfather = parent(it_nodes);
+            it_keys = b_sel1(b_rank1(grandfather) + child_rank);
+            at_end = false;
+        }
+
+        void seek(u_int seek_key){
+            u_int start_index = it_keys;
+            u_int end_index = succ0(it_keys);
+            bool found = false;
+
+            for(u_int i=start_index; i<end_index; i++){
+                if(accessS(i)>=seek_key){
+                    it_keys = i;
+                    u_int child_id = childId(it_keys);
+                    it_nodes = child(it_keys, child_id);
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found){
+                at_end = true;
+            }
+        }
 };
-
-// class Date
-// {
-// private:
-//     int m_year;
-//     int m_month;
-//     int m_day;
- 
-// public:
-//     Date(int year, int month, int day);
- 
-//     void SetDate(int year, int month, int day);
- 
-//     int getYear() { return m_year; }
-//     int getMonth() { return m_month; }
-//     int getDay()  { return m_day; }
-// };
  
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,88 +10,33 @@ using namespace sdsl;
 typedef unsigned int u_int;
 int main()
 {
-    // int_vector<> v = {3,2,1,0,2,1,3,4,1,1,1,3,2,3};
-    // v[1] = 0;
-    // util::bit_compress(v);
-    // cout << v << endl;
-    // cout << size_in_bytes(v) << endl;
-    // cout<<"HOLA"<<endl;
-
-    bit_vector B = {1,1,0,1,1,0,1,1,1,0,1,1,0,0,0,1,1,1,0,0,0,0,1,0,0,1,0,1,0,0};
+    bit_vector B = {1,1,0,1,1,0,1,1,1,0,1,1,0,0,0,1,1,1,0,0,0,0,1,0,0,1,0,1,0,0};   
     //              0 1 2 3 4 5 6 7 8 9
     vector<u_int> S = {0, 0, 1, 3, 3, 4, 5, 4, 5, 6, 8, 9, 2, 5, 2};
 
-    cout<<"hola"<<endl;
     CompactTrie ct = CompactTrie(B,S);
-    // ct.access_S(2);
-    // u_int it = 3;
-    // rank_support_v<1> b_rank1(&B); 
-    // rank_support_v<0> b_rank0(&B);
-    // select_support_mcl<0> b_sel0(&B);
-    // select_support_mcl<1> b_sel1(&B);
 
-    // //key()
-    // u_int key = S[b_rank1(it+1)-1];
-    // cout<<"La key es "<<key<<endl;
+    string instr;
 
-    // //next()
-    // if(it<B.size()-1 && !B[it+1]){
-    //     cout<<"Tiene next"<<endl;
-    //     it++;
-    // }
-    // else{
-    //     cout<<"No tienen next"<<endl;
-    //     //at end = true
-    // }
+    while(true){
+        cin>>instr;
 
-    // //seek(int seekKey)
-    // u_int seekKey = 5;
-    // u_int cant_0 = b_rank0(it+1);
-    // u_int prox_0 = b_sel0(cant_0 + 1);
-
-    // u_int start_index = b_rank1(it+1) - 1;
-    // u_int end_index = b_rank1(prox_0) - 1; //prox_0 - 1 por que el it solo puede llegar hasta ahí, pero +1 por forma del rank
-
-    // bool change = false;
-    // for(int i=start_index; i<=end_index; i++){
-    //     cout<<"el i es "<<i<<endl;
-    //     if(S[i]>=seekKey){
-    //         u_int key = S[b_rank1(i+1)-1];
-    //         it = b_sel1(i+1);
-    //         cout<<"it pasa a ser "<<it<<" con key "<<key<<endl;
-    //         change = true;
-    //     }
-    // }
-
-    // if(!change){
-    //     cout<<"No hay seekkey, paso al final"<<endl;
-    //     //at end
-    // }
-
-    //open
-    // u_int cant_0_0 = b_rank0(it);
-    // it = b_sel0(cant_0_0 + 1) + 1;
-    // cout<<"el nuevo it es "<<it<<endl;
-
-    // //up
-    // // 1. asegurarnos que no estamos en raiz
-    // // 2. calcular padre
-    // //vamos al 0 previo al nivel actual
-    // u_int cant_0 = b_rank0(it);
-    // u_int prev_0 = b_sel0(cant_0);
-
-    // //vamos al parentesis que cierra este, el cual pertenecerá al nivel del padre
-    // u_int cant_1 = b_rank1(prev_0);
-    // u_int prev_1 = b_sel1(cant_1);
-
-    // //vamos al 0 previo al nivel del padre y luego al padre
-    // u_int cant_0_0 = b_rank0(prev_1);
-    // u_int prev_0_0 = b_sel0(cant_0_0);
-
-    // u_int padre = prev_0_0 + 1;
-
-
-
-
-
+        try{
+            if(instr=="open")ct.open();
+            if(instr=="next")ct.next();
+            if(instr=="end")cout<<ct.atEnd()<<endl;
+            if(instr=="key")cout<<ct.key()<<endl;
+            if(instr=="show")ct.showValues();
+            if(instr=="up")ct.up();
+            if(instr=="seek"){
+                int x;
+                cin>>x;
+                ct.seek(x);
+            }
+            if(instr=="close")break;
+        }
+        catch(const char* msg){
+            cerr<<msg<<endl;
+        }
+    }
 }
