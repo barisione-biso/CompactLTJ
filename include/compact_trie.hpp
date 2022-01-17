6bd24178edadd16e1ef628696e5ffa0af49da218
DaniCampos <danielacamposfischer@gmail.com>
2022-01-17 17:47:04 -0300

    Implemented Compact Trie to store the data used by the Compact Trie Iterator

---
+++ include/compact_trie.hpp
@@ -0,0 +1,172 @@
+#ifndef COMPACT_TRIE_H
+#define COMPACT_TRIE_H
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sdsl/vectors.hpp>
+#include <sdsl/wavelet_trees.hpp>
+#include <sdsl/wm_int.hpp>
+#include "utils.hpp"
+
+using namespace std;
+using namespace sdsl;
+
+class CompactTrie{
+    private:
+        bit_vector B;
+        wm_int<> wt;
+
+        rank_support_v<1> b_rank1;
+        rank_support_v<0> b_rank0;
+        select_support_mcl<0> b_sel0;
+        select_support_mcl<1> b_sel1;
+
+        /*
+            Initializes rank and select support for B
+        */
+       void initializeSupport(){
+            util::init_support(b_rank1,&B);
+            util::init_support(b_rank0,&B);
+            util::init_support(b_sel1,&B);
+            util::init_support(b_sel0,&B);
+       }
+
+       // Evaluar si después es mejor recibir los tags como ints
+       int_vector<> turn_into_int_vector(string s){
+           //Parsear string por espacios
+           vector<string> values = parse(s, ' ');
+           //// Crear int_vector para resultados int_vector<> O(n+1);
+           int_vector<> tags(values.size());
+           
+           //convertir cada termino parseado en un entero y guardarlo en el vector
+           int i = 0;
+           for(auto v: values){
+               tags[i++] = stoi(v);
+           }
+           return tags;
+       }
+
+    public:
+
+        CompactTrieIterator(string file_name){
+            loadFromFile(file_name);
+        };
+
+        CompactTrie(bit_vector b, string s){
+            B = b;
+            construct_im(wt, turn_into_int_vector(s));
+            initializeSupport();
+        }
+
+        /*
+            Destructor
+        */
+        ~CompactTrie(){};
+
+        /*
+            Recives index in bit vector
+            Returns index of next 0
+        */
+        uint64_t succ0(uint64_t it){
+            uint64_t cant_0 = b_rank0(it);
+            return b_sel0(cant_0 + 1);
+        }
+        
+        /*
+            Recives index in bit vector
+            Returns index of previous 0
+        */
+        uint64_t prev0(uint64_t it){
+            uint64_t cant_0 = b_rank0(it);
+            return b_sel0(cant_0);
+        }
+
+        /*
+            Recives index of current node and the child that is required
+            Returns index of the nth child of current node
+        */
+        uint64_t child(uint64_t it, uint64_t n){
+            return b_sel0(b_rank1(it+n)) + 1;
+        }
+
+        /*
+            Recives index of node whos children we want to count
+            Returns how many children said node has
+        */
+        uint64_t childrenCount(uint64_t it){
+            return succ0(it) - it;
+        }
+
+        /*
+            Recives node index
+            Returns index of position in parent
+        */
+        uint64_t getPosInParent(uint64_t it){
+            return b_sel1(b_rank0(it));
+        }
+
+        /*
+            Recives index of node
+            Return which child of its parent it is
+        */
+        uint64_t childRank(uint64_t it){
+            uint64_t pos = getPosInParent(it);
+            return pos - prev0(pos);
+        }
+
+        /*
+            Recives index of node
+            Returns index of parent node
+        */  
+        uint64_t parent(uint64_t it){
+            uint64_t pos = getPosInParent(it);
+            return prev0(pos) + 1;
+        }
+        
+        /*
+            Stores Compact Trie Iterator to file saving the size of B, B and S.
+        */
+        void storeToFile(string file_name){
+            ofstream stream(file_name);
+            if(stream.is_open()){
+                stream<<B.size()<<'\n';
+                for(uint64_t i=0; i<B.size(); i++){
+                    stream<<B[i]<<" ";
+                }
+                stream<<'\n';
+                for(uint64_t i=0; i<wt.size(); i++){
+                    stream<<wt[i]<<" ";
+                }
+            }
+            stream.close();
+        }
+
+        /*
+            Loads Compact Trie from file restoring B and the Wavelet Tree
+        */
+        void loadFromFile(string file_name){
+            ifstream stream(file_name);
+            uint64_t B_size;
+            string s;
+            uint64_t val;
+
+            if(stream.is_open()){
+                stream>>B_size;
+                B = bit_vector(B_size);
+                for(uint64_t i=0; i<B_size; i++){
+                    stream>>val;
+                    B[i] = val;
+                }
+                stream.ignore(numeric_limits<streamsize>::max(),'\n');
+                getline(stream, s);
+            }
+            stream.close();
+            
+            construct_im(wt, s, 'd');
+            initializeSupport();
+        }
+};
+
+#endif
