2f8c750f23021795040f99335a92a88340778506
Fabrizio Barisione <fbarisio@dijkstra.inf.santiago.usm.cl>
2023-02-10 08:38:17 -0300

    Ready to test

---
+++ run_query_dijkstra.sh
@@ -0,0 +1,1 @@
+./build/query_index queries_filtered.txt /data/graphs/wikidata/wikidata-filtered.nt.sorted.enumerated-/ 1000
--- src/query_index.cpp
+++ src/query_index.cpp
@@ -83,8 +83,8 @@ int main(int argc, char* argv[]){
                 stop = high_resolution_clock::now();
                 time_span = duration_cast<microseconds>(stop - start);
                 total_time = time_span.count();
-                cout << query_number <<  ";" << number_of_results << ";" << (unsigned long long)(total_time*1000000000ULL) << ";" << ltj.get_gao() << endl;
-                //cout << query_number <<  ";" << number_of_results << ";" << (unsigned long long)(total_time*1000000000ULL) << endl;
+                //cout << query_number <<  ";" << number_of_results << ";" << (unsigned long long)(total_time*1000000000ULL) << ";" << ltj.get_gao() << endl;
+                cout << query_number <<  ";" << number_of_results << ";" << (unsigned long long)(total_time*1000000000ULL) << endl;
                 query_number++; 
 
                 variable_tuple_mapping.clear();
